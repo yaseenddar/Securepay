0d2f58f326102baf0eb1d7b1625bb310e63972f3
yaseen@12 <yaseen@gmail.com>
2026-03-24 23:21:07 +0530

    first commit

---
+++ src/main/java/com/securepay/app/Securepay/SecurepayApplication.java
@@ -0,0 +1,13 @@
+package com.securepay.app.Securepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurepayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurepayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securepay/app/Securepay/SecurepayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securepay.app.Securepay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurepayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
